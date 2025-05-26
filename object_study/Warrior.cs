1f76d4c034bb67ce69aedd4f95ba7a9d7894a29e
Dyna-Rise <dynarise.headquarters@gmail.com>
2025-05-26 12:03:36 +0900

    Warriorクラス上げ直し

---
+++ object_study/Warrior.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_study
+{
+    internal class Warrior : Player
+    {
+        public Warrior(string name) : base(name)
+        {
+            hp = 120;
+            attack = 20;
+            JobSet("戦士");
+        }
+
+        public void HardAttack()
+        {
+            Console.WriteLine("両手持ち攻撃！");
+        }
+
+    }
+}
